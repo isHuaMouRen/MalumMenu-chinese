f172a905f32b8a917d5ea583116e605f64858524
HuaMouRen <2825338723@qq.com>
2026-06-15 19:23:00 +0800

    feat(cn): 完成聊天标签页汉化

--- src/UI/Windows/Tabs/ChatTab.cs
+++ src/UI/Windows/Tabs/ChatTab.cs
@@ -4,7 +4,7 @@ namespace MalumMenu;
 
 public class ChatTab : ITab
 {
-    public string name => "Chat";
+    public string name => "聊天";
 
     public void Draw()
     {
@@ -21,21 +21,21 @@ public class ChatTab : ITab
 
     private void DrawGeneral()
     {
-        CheatToggles.enableChat = GUILayout.Toggle(CheatToggles.enableChat, " Enable Chat");
+        CheatToggles.enableChat = GUILayout.Toggle(CheatToggles.enableChat, " 启用聊天");
 
-        CheatToggles.bypassUrlBlock = GUILayout.Toggle(CheatToggles.bypassUrlBlock, " Bypass URL Block");
+        CheatToggles.bypassUrlBlock = GUILayout.Toggle(CheatToggles.bypassUrlBlock, " 绕过URL屏蔽");
 
-        CheatToggles.lowerRateLimits = GUILayout.Toggle(CheatToggles.lowerRateLimits, " Lower Rate Limits");
+        CheatToggles.lowerRateLimits = GUILayout.Toggle(CheatToggles.lowerRateLimits, " 降低发送频率限制");
     }
 
     private void DrawTextbox()
     {
-        GUILayout.Label("Textbox", GUIStylePreset.TabSubtitle);
+        GUILayout.Label("输入框", GUIStylePreset.TabSubtitle);
 
-        CheatToggles.unlockCharacters = GUILayout.Toggle(CheatToggles.unlockCharacters, " Unlock Extra Characters");
+        CheatToggles.unlockCharacters = GUILayout.Toggle(CheatToggles.unlockCharacters, " 解锁额外字符");
 
-        CheatToggles.longerMessages = GUILayout.Toggle(CheatToggles.longerMessages, " Allow Longer Messages");
+        CheatToggles.longerMessages = GUILayout.Toggle(CheatToggles.longerMessages, " 允许更长的消息");
 
-        CheatToggles.unlockClipboard = GUILayout.Toggle(CheatToggles.unlockClipboard, " Unlock Clipboard");
+        CheatToggles.unlockClipboard = GUILayout.Toggle(CheatToggles.unlockClipboard, " 解锁剪贴板");
     }
 }
